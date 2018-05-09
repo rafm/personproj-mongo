8d62e64b1d7c97a008aea815f141718872796c44
rafm <rafm@github.com>
2018-05-09 17:16:20 -0300

    Fixing person entity mapping

--- src/main/java/com/daitangroup/initproj/model/Person.java
+++ src/main/java/com/daitangroup/initproj/model/Person.java
@@ -1,14 +1,18 @@
 package com.daitangroup.initproj.model;
 
-//import javax.persistence.Entity;
-//import javax.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 
-//@Entity
+@Entity
 public class Person {
 
-//	@Id
+	@Id
+	@GeneratedValue
 	private Integer id;
 	
+	@NotNull
 	private String name;
 	
 	private String country;
