49febf0eb8f2ae828430b373b9487ef0e099bbad
Rafael Augusto <rafael.augusto.fm@outlook.com>
2018-05-11 13:52:14 -0300

    Annotating repository classes with Spring's @Repository

--- src/main/java/com/daitangroup/initproj/repository/PersonRepository.java
+++ src/main/java/com/daitangroup/initproj/repository/PersonRepository.java
@@ -1,7 +1,9 @@
 package com.daitangroup.initproj.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import com.daitangroup.initproj.model.Person;
 
+@Repository
 public interface PersonRepository extends MongoRepository<Person, String> {}
