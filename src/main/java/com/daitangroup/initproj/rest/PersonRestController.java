5ff208f67776e976509f089f605710220c3fe5e9
Rafael Augusto <rafael.augusto.fm@outlook.com>
2018-05-18 10:33:41 -0300

    Removing business rule logic from presentation/rest layer and mongodb search endpoint

--- src/main/java/com/daitangroup/initproj/rest/PersonRestController.java
+++ src/main/java/com/daitangroup/initproj/rest/PersonRestController.java
@@ -9,7 +9,7 @@ import javax.validation.constraints.Min;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,33 +22,26 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.daitangroup.initproj.model.Person;
-import com.daitangroup.initproj.repository.elasticsearch.PersonElasticsearchRepository;
-import com.daitangroup.initproj.repository.mongo.PersonMongoRepository;
+import com.daitangroup.initproj.service.PersonService;
 
 @RestController
 @RequestMapping("/person")
 public class PersonRestController {
 
-	@Autowired private PersonMongoRepository personMongoRepository;
-
-	@Autowired private PersonElasticsearchRepository personElasticsearchRepository;
+	@Autowired private PersonService personService;
 	
-	@GetMapping("/elastic")
-	public Iterable<Person> findInElasticsearch() {
-		return personElasticsearchRepository.findAll();
-	}
-	
+	// TODO Fix sorting
 	@GetMapping
 	public Iterable<Person> findAll(@RequestParam(defaultValue="0") @Min(0) int page,
-			@RequestParam(defaultValue="20") @Min(1) @Max(100) int size,
-			@RequestParam(defaultValue="ASC") Direction sort,
-			@RequestParam(defaultValue="name") String... sortingFields) {
-		return personMongoRepository.findAll(PageRequest.of(page, size, sort, sortingFields)).getContent();
+									@RequestParam(defaultValue="20") @Min(1) @Max(100) int size) {
+		return personService.search(new NativeSearchQueryBuilder()
+											.withPageable(PageRequest.of(page, size))
+											.build());
 	}
 	
 	@GetMapping("/{id}")
 	public ResponseEntity<Person> findById(@PathVariable String id) {
-		Optional<Person> result = personMongoRepository.findById(id);
+		Optional<Person> result = personService.findById(id);
 		return result.isPresent() ? ResponseEntity.ok(result.get()) : ResponseEntity.notFound().build();
 	}
 	
@@ -58,8 +51,8 @@ public class PersonRestController {
 			return ResponseEntity.badRequest().build();
 		}
 
-		personMongoRepository.save(person);
-		personElasticsearchRepository.save(person);
+		personService.registerOrUpdate(person);
+		
 		return ResponseEntity.created(URI.create("/person/"+person.getId())).build();
 	}
 	
@@ -67,25 +60,18 @@ public class PersonRestController {
 	// TODO ETag HTTP 204
 	@PutMapping("/{id}")
 	public ResponseEntity<String> update(@PathVariable String id, @RequestBody @Valid Person person) {
-		boolean isUpdatingPerson = personMongoRepository.existsById(id);
+		boolean isUpdatingPerson = personService.existsById(id);
 		
 		person.setId(id);
+		personService.registerOrUpdate(person);
 
-		personMongoRepository.save(person);
-		personElasticsearchRepository.save(person);
 		return isUpdatingPerson ? ResponseEntity.noContent().build() : ResponseEntity.created(URI.create("/person/"+person.getId())).build();
 	}
 	
 	// TODO Concurrency
 	@DeleteMapping("/{id}")
 	public ResponseEntity<String> delete(@PathVariable String id) {
-		if (personMongoRepository.existsById(id)) {
-			personMongoRepository.deleteById(id);
-		}
-		
-		if (personElasticsearchRepository.existsById(id)) {
-			personElasticsearchRepository.deleteById(id);
-		}
+		personService.remove(id);
 		
 		return ResponseEntity.ok().build();
 	}
