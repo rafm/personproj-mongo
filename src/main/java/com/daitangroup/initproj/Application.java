5349040ab3652c447d7f69fe69c5615c707e6be8
Rafael Augusto <rafael.augusto.fm@outlook.com>
2018-05-21 18:26:54 -0300

    Changing MongoDB data population strategy for development use from Application Java file to data.json file

--- src/main/java/com/daitangroup/initproj/Application.java
+++ src/main/java/com/daitangroup/initproj/Application.java
@@ -1,42 +1,12 @@
 package com.daitangroup.initproj;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.daitangroup.initproj.model.Person;
-import com.daitangroup.initproj.repository.elasticsearch.PersonElasticsearchRepository;
-import com.daitangroup.initproj.repository.mongo.PersonMongoRepository;
-
 @SpringBootApplication
-public class Application implements CommandLineRunner {
-
-	@Autowired private PersonMongoRepository personMongoRepository;
-
-	@Autowired private PersonElasticsearchRepository personElasticsearchRepository;
+public class Application {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-	}
-
-	@Override
-	public void run(String... args) {
-		personMongoRepository.deleteAll();
-		personElasticsearchRepository.deleteAll();
-		
-		personElasticsearchRepository.save(
-			personMongoRepository.save(new Person("Joaquim", "BR"))
-		);
-		
-		personElasticsearchRepository.save(
-			personMongoRepository.save(new Person("Bran", "US"))
-		);
-		personElasticsearchRepository.save(
-			personMongoRepository.save(new Person("Yushin", "JP"))
-		);
-		personElasticsearchRepository.save(
-			personMongoRepository.save(new Person("Pablo", "ES"))
-		);
 	}
 }
--- src/main/java/com/daitangroup/initproj/repository/mongo/MongoConfiguration.java
+++ src/main/java/com/daitangroup/initproj/repository/mongo/MongoConfiguration.java
@@ -3,7 +3,10 @@ package com.daitangroup.initproj.repository.mongo;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.data.repository.init.Jackson2RepositoryPopulatorFactoryBean;
 
 import com.mongodb.MongoClient;
 
@@ -17,5 +20,12 @@ public class MongoConfiguration {
 	@Bean
 	public MongoClient mongoClient() {
 		return new MongoClient(host);
+	}
+	
+	@Bean
+	public Jackson2RepositoryPopulatorFactoryBean repositoryPopulator() {
+		Jackson2RepositoryPopulatorFactoryBean factory = new Jackson2RepositoryPopulatorFactoryBean();
+		factory.setResources(new Resource[] {new ClassPathResource("data.json")});
+		return factory;
 	}
 }
